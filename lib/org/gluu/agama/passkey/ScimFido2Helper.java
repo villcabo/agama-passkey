27c9787a7c368283988d8a79169cd51bcfe568de
Bismarck Villca <bismarck.villca@gmail.com>
2024-02-13 23:46:28 -0400

    feat: update item completed
    Signed-off-by: Bismarck Villca <bismarck.villca@gmail.com>

--- lib/org/gluu/agama/passkey/ScimFido2Helper.java
+++ lib/org/gluu/agama/passkey/ScimFido2Helper.java
@@ -8,9 +8,12 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.*;
 
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+
 public class ScimFido2Helper extends ScimWSBase {
 
     private static final String SCOPE_FIDO2_READ = SCOPE_PREFIX + "/fido2.read";
+    private static final String SCOPE_FIDO2_WRITE = SCOPE_PREFIX + "/fido2.write";
 
     public ScimFido2Helper() throws IOException {
         super(false, null);
@@ -18,13 +21,12 @@ public class ScimFido2Helper extends ScimWSBase {
 
     public ScimFido2Helper(ScimSetting scimSetting) throws IOException {
         super(true, scimSetting);
-        setScope(SCOPE_FIDO2_READ);
+        setScope(SCOPE_FIDO2_READ + " " + SCOPE_FIDO2_WRITE);
     }
 
     public Map<String, Object> getFidoDeviceByUser(String userInum) throws IOException {
         try {
             URL url = new URL(apiBase + "/v2/Fido2Devices");
-            log.debug("Scim Fido2Devices url: {}", url);
             HTTPRequest request = new HTTPRequest(HTTPRequest.Method.GET, url);
             request.setAccept("application/json");
 
@@ -43,6 +45,7 @@ public class ScimFido2Helper extends ScimWSBase {
                 while (i < jsonArray.length()) {
                     JSONObject item = jsonArray.getJSONObject(i);
                     Map<String, String> result = new HashMap<>();
+                    result.put("id", item.getString("id"));
                     result.put("displayName", item.has("displayName") ? item.getString("displayName") : "Registered without name");
                     result.put("creationDate", item.getString("creationDate"));
                     mapList.add(result);
@@ -53,6 +56,25 @@ public class ScimFido2Helper extends ScimWSBase {
 
         } catch (Exception e) {
             throw new IOException("Could not obtain the user's list of fido devices: " + userInum, e);
+        }
+    }
+
+    public String updateDevice(String userId, String deviceId, String displayName) throws Exception {
+        try {
+            URL url = new URL(apiBase + "/v2/Fido2Devices/" + encode(deviceId));
+            HTTPRequest request = new HTTPRequest(HTTPRequest.Method.PUT, url);
+            request.setAccept(APPLICATION_JSON);
+            request.setContentType(APPLICATION_JSON);
+            String body = net.minidev.json.JSONObject.toJSONString(Map.of("userId", userId, "displayName", displayName));
+            log.debug("Request updateDevice body: {}", body);
+            request.setQuery(body);
+
+            String response = sendRequest(request, true, true).getContentAsJSONObject().toJSONString();
+            log.debug("Response update device: {}", response);
+            return "UPDATED";
+
+        } catch (Exception e) {
+            throw new IOException("Could not update device: " + e.getMessage(), e);
         }
     }
 }
