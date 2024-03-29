1ca10dc01064596dc0647e35f3b75ee2a0e1c72e
Bismarck Villca <bismarck.villca@gmail.com>
2024-03-28 23:19:02 -0400

    feat: remove agama-inbound
    Signed-off-by: Bismarck Villca <bismarck.villca@gmail.com>

--- lib/org/gluu/agama/passkey/IdentityProcessor.java
+++ lib/org/gluu/agama/passkey/IdentityProcessor.java
@@ -9,16 +9,18 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Map;
 
-import static io.jans.inbound.Attrs.*;
-
 public class IdentityProcessor {
 
     private static final Logger log = LoggerFactory.getLogger(IdentityProcessor.class);
 
     private static final String INUM_ATTR = "inum";
+    private static final String UID = "uid";
+    private static final String GIVEN_NAME = "givenName";
+    private static final String DISPLAY_NAME = "displayName";
+    private static final String MAIL = "mail";
 
     public static Map<String, String> accountFromUid(String uid) throws InvalidClaimException {
-        User user = getUser(UID, uid);
+        User user = getUser("uid", uid);
         boolean local = user != null;
         log.debug("There is {} local account for {}", local ? "a" : "no", uid);
 
